20a17d97100418e1d409dceea2b1de2c49b2259c
Jesse Hanson <jesse@mobilecart.io>
2017-02-08 22:25:45 -0500

    add config for theme, after removing from core bundle

--- MobileCartAdminBundle.php
+++ MobileCartAdminBundle.php
@@ -6,4 +6,14 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MobileCartAdminBundle extends Bundle
 {
+    public function boot()
+    {
+        $this->container->get('cart.theme.config')
+            ->setTheme(
+                'admin',
+                'MobileCartAdminBundle::admin-layout.html.twig',
+                'MobileCartAdminBundle:',
+                'bundles/mobilecartadmin'
+            );
+    }
 }
